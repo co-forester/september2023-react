816f47b6e1cc03b37ad03b21360ff402abaa5ec7
SerGio <foresterares@gmail.com>
2024-01-28 00:46:24 +0200

    and lesson1

--- src/components/User.js
+++ src/components/User.js
@@ -3,12 +3,10 @@ import React from 'react';
 const User = ({item}) => {
     // console.log (props);
     // let {name, age} = props;
-    const {name, age, image} = item;
+    const {name} = item;
     return (
         <div>
            <div>name: {name}</div>
-           <div>age: {age}</div>
-           <img src={image} alt={name}/>
         </div>
     );
 };
