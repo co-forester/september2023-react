ad834d6fc4c68be939bcc44af01d4ff9566bb441
SerGio <foresterares@gmail.com>
2024-01-28 23:53:07 +0200

    add constants & services & UserDetails.js

--- src/components/UserContainer/UserDetails/UserDetails.js
+++ src/components/UserContainer/UserDetails/UserDetails.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+import css from './UserDetails.module.css'
 const UserDetails = ({userDetails}) => {
    const {id, name, username, email} = userDetails
     return (
-        <div>
+        <div className={css.UserDetails}>
            <div>id: {id}</div>
            <div>name: {name}</div>
            <div>username: {username}</div>
