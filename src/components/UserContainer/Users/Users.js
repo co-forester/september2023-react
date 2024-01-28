21dcebbe18f2af31b40e54c6ef2a533465548e2a
SerGio <foresterares@gmail.com>
2024-01-28 22:07:23 +0200

    add axios

--- src/components/UserContainer/Users/Users.js
+++ src/components/UserContainer/Users/Users.js
@@ -2,13 +2,15 @@ import React, {useEffect, useState} from 'react';
 
 import css from './Users.module.css'
 import {User} from "../User/User.js";
+import axios from "axios";
 
 const Users = () => {
     const [users, setUsers] = useState([]);
     useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(res=> res.json())
-            .then(value => setUsers(value))
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(({data}) => setUsers(data))
+
     }, [])
     return (
         <div className={css.Users}>
