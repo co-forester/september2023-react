ba1978be20ad8499f10cfc11e7097c4f864e20aa
SerGio <foresterares@gmail.com>
2024-01-30 06:59:06 +0200

    change css

--- src/constants/Launch/Launch.js
+++ src/constants/Launch/Launch.js
@@ -6,7 +6,7 @@ const Launch = ({launch}) => {
    const {mission_name, launch_year, links:{mission_patch_small}} = launch;
     return (
         <div className={css.Launch}>
-           <div>mission_name: {mission_name}</div>
+           <div className={css.MissionNameLaunch}>mission_name: {mission_name}</div>
            <div>launch_year: {launch_year}</div>
             <img src={mission_patch_small} alt={mission_name}/>
         </div>
