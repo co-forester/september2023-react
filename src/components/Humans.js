c60607ab54048e413ab64b9fe848cb24139736ab
SerGio <foresterares@gmail.com>
2024-01-31 21:14:36 +0200

    changed the 'feth' on to folder 'data'

--- src/components/Humans.js
+++ src/components/Humans.js
@@ -1,14 +1,15 @@
 import React, {useEffect, useState} from 'react';
 
 import {Human} from "./Human";
+import {humans} from '../data/characters'
 
 const Humans = () => {
-    const [humans, setHumans] = useState([]);
-    useEffect(()=>{
-        fetch('https://rickandmortyapi.com/api/character')
-            .then(value => value.json())
-            .then(({results})=>setHumans(results))
-    }, [])
+    // const [humans, setHumans] = useState([]);
+    // useEffect(()=>{
+    //     fetch('https://rickandmortyapi.com/api/character')
+    //         .then(value => value.json())
+    //         .then(({results})=>setHumans(results))
+    // }, [])
     return (
         <div>
             {humans.map(human=><Human key={human.id} human={human}/>)}
---
+++ src/data/characters.js
@@ -0,0 +1,53 @@
+let humans = [
+    {
+        id: 1,
+        name: "Rick Sanchez",
+        status: "Alive",
+        species: "Human",
+        gender: "Male",
+        image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+    },
+    {
+        id: 2,
+        name: "Morty Smith",
+        status: "Alive",
+        species: "Human",
+        gender: "Male",
+        image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+    },
+    {
+        id: 15,
+        name: "Alien Rick",
+        status: "unknown",
+        species: "Alien",
+        gender: "Male",
+        image: "https://rickandmortyapi.com/api/character/avatar/15.jpeg"
+    },
+    {
+        id: 20,
+        name: "Ants in my Eyes Johnson",
+        status: "unknown",
+        specie: "Human",
+        gender: "Male",
+        image: "https://rickandmortyapi.com/api/character/avatar/20.jpeg"
+    },
+    {
+        id: 12,
+        name: "Alexander",
+        status: "Dead",
+        species: "Human",
+        gender: "Male",
+        image: "https://rickandmortyapi.com/api/character/avatar/12.jpeg"
+    },
+    {
+        id: 11,
+        name: "Albert Einstein",
+        status: "Dead",
+        species: "Human",
+        gender: "Male",
+        image: "https://rickandmortyapi.com/api/character/avatar/11.jpeg"
+    }
+]
+export {
+    humans
+}
