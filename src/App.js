b2008b91ea6464f1b77f2a9f497d09370231ce64
SerGio <foresterares@gmail.com>
2024-02-01 10:21:50 +0200

    changed font

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import {useState} from "react";
 
 const App = () => {
+    // const [login, setLogin] = useState('default');
+    // const [password, setPassword] = useState();
+    const [formState, setFormState] = useState({login: '', password: ''})
+    function onSubmit(e){
+        console.log(e.target.login.value);
+        console.log(e.target.password.value);
+        e.preventDefault();
+        console.log(formState)
+    }
+    // function onLoginChange(e){
+    //     setLogin(e.target.value)
+    // }function onPasswordChange(e){
+    //     setPassword(e.target.value)
+    // }
+    function onChange(e) {
+        // console.log({[e.target.name]: e.target.value});
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
     return (
         <div>
-            App
+           <form onSubmit={onSubmit}>
+               <input type={"text"} name={'login'} value={formState.login} onChange={onChange}/>
+               <input type={"text"} name={'password'} value={formState.password} onChange={onChange}/>
+               <button>submit</button>
+           </form>
         </div>
     );
 };
