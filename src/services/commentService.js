8185cf4788a79bf2495182c950f0797169cf6ccf
SerGio <foresterares@gmail.com>
2024-02-02 14:20:12 +0200

    change commentService.js

--- src/services/commentService.js
+++ src/services/commentService.js
@@ -3,8 +3,10 @@ import {urls} from "../constants/urls";
 
 
 const commentService = {
-    getAll:()=>axiosService.get(urls.comments.base),
-    create:(data)=>axiosService.post(urls.comments.base, data)
+    getAll:()=> axiosService.get(urls.comments.base),
+    create:(data)=> axiosService.post(urls.comments.base, data),
+    updateById: (id, data) => axiosService.put(urls.comments.byId(id), data),
+    deleteById: (id) => axiosService.delete(urls.comments.byId(id))
 }
 
 export {
