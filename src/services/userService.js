1226cd4b1cd72b17402f0366232093c325a04570
SerGio <foresterares@gmail.com>
2024-02-02 14:23:46 +0200

    change userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,7 +4,9 @@ import {urls} from "../constants/urls";
 
 const userService = {
     getAll:()=>axiosService.get(urls.users.base),
-    create:(data)=>axiosService.post(urls.users.base, data)
+    create:(data)=>axiosService.post(urls.users.base, data),
+    updateById:(id, data) => axiosService.put(urls.users.byId(id), data),
+    deleteById:(id) => axiosService.delete(urls.users.byId(id))
 }
 
 export {
