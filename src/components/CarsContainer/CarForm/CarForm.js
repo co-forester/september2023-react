0c51f27c4481a0356a7a3fb5d5829633306d83b3
SerGio <foresterares@gmail.com>
2024-02-03 08:53:03 +0200

    create const save in CarForm.js

--- src/components/CarsContainer/CarForm/CarForm.js
+++ src/components/CarsContainer/CarForm/CarForm.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {useForm} from "react-hook-form";
-import {create} from "axios";
 
 import {carService} from "../../../services/carService";
 
@@ -9,13 +8,13 @@ const CarForm = ({setCars}) => {
     const {reset, handleSubmit,register}= useForm();
 
     const save = async (car) => {
-        await carService.create(data);
-
+        const {data} = await carService.create(car);
+        setCars(prev=>[...prev, data]);
+        reset()
     }
 
     return (
         <form onSubmit={handleSubmit(save)}>
-            <input type={"number"} placeholder={'id'} {...register('id')}/>
             <input type={"text"} placeholder={'brand'} {...register('brand')}/>
             <input type={"number"} placeholder={'price'} {...register('price')}/>
             <input type={"number"} placeholder={'year'} {...register('year')}/>
