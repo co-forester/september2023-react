1023cf7289bcc5af96cbafa8724081f631332216
SerGio <foresterares@gmail.com>
2024-02-08 13:57:59 +0200

    create Albums.js

--- src/components/AlbumsContainer/Albums/Albums.js
+++ src/components/AlbumsContainer/Albums/Albums.js
@@ -1,9 +1,17 @@
+import {useEffect, useState} from "react";
+import {albumService} from "../../../services/AlbumService";
+import {Album} from "../Album/Album";
 
 
 const Albums = () => {
+    const [albums, setAlbums] = useState([]);
+
+    useEffect(()=>{
+        albumService.getAll().then(({data})=>setAlbums(data))
+    }, [])
     return (
         <div>
-            Albums
+            {albums.map(album=><Album key={album.id} album={album}/>)}
         </div>
     );
 };
