d9934294c0699d54534ef3ef5734388f1979ddaa
SerGio <foresterares@gmail.com>
2024-02-09 11:42:21 +0200

    add css in the MainLayout.js

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {Header} from "../components/Header";
 import {Outlet} from "react-router-dom";
 
+import css from './MainLayout.module.css'
+
 const MainLayout = () => {
     return (
-        <div>
+        <div className={css.MainLayout}>
             <Header/>
             <Outlet/>
         </div>
