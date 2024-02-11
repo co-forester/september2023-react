7729e019e243c04c1f339bbd1d2733c5114e9ccc
SerGio <foresterares@gmail.com>
2024-02-11 02:14:26 +0200

    add useNavigate()

--- src/components/UserContainer/UserContainer.js
+++ src/components/UserContainer/UserContainer.js
@@ -1,18 +1,12 @@
 import {useEffect, useState} from "react";
-import {useLocation} from "react-router-dom";
 
 import {userService} from "../../services/userService";
 import {Users} from "./Users/Users";
 import {UserDetails} from "../UserDetails/UserDetails";
 
-const UserContainer = () => {
+const UserContainer = ({user}) => {
     const [users, setUsers] = useState([]);
-    const {state:{id}} = useLocation();
-    const [user, setUser] = useState(null);
 
-    useEffect(async () => {
-       await userService.getById(id).then(({data}) => setUser(data))
-    }, [id])
 
     useEffect(async () => {
         await userService.getAll().then(({data}) => setUsers(data))
--- src/pages/UsersPages.js
+++ src/pages/UsersPages.js
@@ -1,9 +1,18 @@
 import {UserContainer} from "../components/UserContainer/UserContainer";
+import {useLocation} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {userService} from "../services/userService";
 
 const UsersPages = () => {
+    const {state:{id}} = useLocation();
+    const [user, setUser] = useState(null);
+
+    useEffect(async () => {
+        await userService.getById(id).then(({data}) => setUser(data))
+    }, [id])
     return (
         <div>
-            <UserContainer/>
+            <UserContainer user={user}/>
         </div>
     );
 };
