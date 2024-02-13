546666ca03a7fadf85920a3cc0a7c8dff6441f00
SerGio <foresterares@gmail.com>
2024-02-13 17:30:08 +0200

    corrected Todo.js (add toSring() (варіант №2)

--- src/components/TodosContainers/Todo/Todo.js
+++ src/components/TodosContainers/Todo/Todo.js
@@ -8,6 +8,7 @@ const Todo = ({todo}) => {
             <div>userId: {userId}</div>
             <div>id: {id}</div>
             <div>title: {title}</div>
+            {/*<div>completed: {completed ? 'true' : 'false'}</div>*/}
             <div>completed: {completed.toString()}</div>
         </div>
     );
