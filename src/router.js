7579d4c7a27c5a251829b528b6798513c2626c31
SerGio <foresterares@gmail.com>
2024-02-20 00:19:24 +0200

    create router.js

--- src/router.js
+++ src/router.js
@@ -1,6 +1,15 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom";
+
+import {MainLayout} from "./layouts";
+import {CharactersPage, EpisodesPage} from "./pages";
 
 const router = createBrowserRouter([
+    {path: '', element: <MainLayout/>, children:[
+            {index: true, element: <Navigate to={'episodes'}/>},
+            {path: 'episodes', element: <EpisodesPage/>, children:[
+                    {path: 'characters', element: <CharactersPage/>}
+                ]}
+        ]}
 
 ])
 
