72b82306f685a74c35034ba1a6a5a084f7ff1ac4
SerGio <foresterares@gmail.com>
2024-02-20 16:13:13 +0200

    change characterService.js, Characters.js

--- src/components/CharactersContainer/Characters/Characters.js
+++ src/components/CharactersContainer/Characters/Characters.js
@@ -1,19 +1,21 @@
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
+
 import {characterService} from "../../../services";
 
 const Characters = () => {
     const {ids} = useParams();
     const [characters, setCharacters] = useState([]);
     // console.log(ids);
+    // console.log(characters);
 
     useEffect(() => {
-        characterService.
-    }, []);
+        characterService.getByIds(ids).then(({data}) => setCharacters(data))
+    }, [ids]);
 
     return (
         <div>
-            Characters
+            characters.map(character)
         </div>
     );
 };
