d479adc7b22e21578fa1604b24264928b931c1cc
SerGio <foresterares@gmail.com>
2024-02-22 10:57:27 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import ReactDOM from 'react-dom/client';
-
-import {App} from './App';
+import {router} from "./router";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <RouterProvider router={router} />
 );
 
---
+++ src/router.js
@@ -0,0 +1,7 @@
+const router = createBrowserRouter([
+    {
+
+    }
+]);
+
+export {router}
