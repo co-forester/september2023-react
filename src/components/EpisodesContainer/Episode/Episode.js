3bb44fa0b3cdd9e36b6c19c72350a96be299af25
SerGio <foresterares@gmail.com>
2024-03-08 15:31:07 +0200

    create EpisodesPagination.js.js

--- src/components/EpisodesContainer/Episode/Episode.js
+++ src/components/EpisodesContainer/Episode/Episode.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import css from './Episode.module.css'
+import {useNavigate} from "react-router-dom";
 
 const Episode = ({episode}) => {
-    const {id, name, episode: chapter} = episode
+    const {id, name, episode: chapter, characters} = episode;
+    const navigate = useNavigate();
+
+    const ids = characters.map(characters => characters.split('/').splice(-1)[0]).join();
 
     return (
-        <div className={css.Episode}>
+        <div onClick={()=>navigate(`/characters/${ids}`)} className={css.Episode}>
             <div>id: {id}</div>
             <div>name: {name}</div>
             <div>chapter: {chapter}</div>
--- src/components/EpisodesContainer/EpisodesPagination/EpisodesPagination.js
+++ src/components/EpisodesContainer/EpisodesPagination/EpisodesPagination.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import {useSelector} from "react-redux";
+import {useSearchParams} from "react-router-dom";
 
 const EpisodesPagination = () => {
     const {prev, next} = useSelector (state => state.episodes);
+    const [, setQuery] = useSearchParams({page:'1'})
 
+    const prevPage = () => {
+        setQuery( prev => {
+            prev.set('page', `${+prev.get('page')-1}`);
+            return prev
+        })
+    }
+
+    const nextPage = () => {
+        setQuery( prev => {
+            prev.set('page', (+prev.get('page')+1).toString());
+            return prev
+        })
+    }
 
     return (
         <div>
-            <button>prev page</button>
-            <button>next page</button>
+            <button disabled={!prev} onClick={prevPage}>prev page</button>
+            <button disabled={!next} onClick={nextPage}>next page</button>
         </div>
     );
 };
